55ec353249a41a84144a47f2732a37fe94f61643
kwamebrowne <theeecryptoking@gmail.com>
2023-06-06 12:20:55 -0500

    testing how data flows

---
+++ lib/debug.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+# from lib.player import *
+
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///game.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    print("this db is awakend")
+
+    import ipdb; ipdb.set_trace()
